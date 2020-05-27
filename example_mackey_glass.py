d5e97240aa15f8e211975f1320656caac2de5341
AleixEF <65950800+AleixEF@users.noreply.github.com>
2020-05-27 13:17:08 +0200

    Examples of how to use the esn classes
    This two files are two examples of how to use the standard echo state network class.

---
+++ example_mackey_glass.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 10:55:05 2020
+
+@author: aleix
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from echo_state_networks.standard import StdEsn
+
+time = np.loadtxt("ExampleSignals/mackey_glass.txt", usecols=0)
+mackey_glass = np.loadtxt("ExampleSignals/mackey_glass.txt", usecols=1)
+
+pred_length = 1000 #we will predict 1000 points in the future.
+#the time step for this signal is 1, so we know the future time instants:
+time_future = [time[-1] + i for i in range(1, pred_length+1)] 
+
+plt.figure()
+plt.xlabel("time")
+plt.title("Mackey glass training signal")
+plt.plot(time, mackey_glass)
+
+#Creation of the ESN with default parameters
+esn = StdEsn()
+
+#Training
+#we add some noise to the mackey glass training signal as regularization
+noise = np.random.uniform(0, 1e-6, size=len(mackey_glass)) 
+train_signal = mackey_glass + noise
+xstates, mse = esn.teacher_forcing(train_signal) 
+
+print("Training mean squared error = ", mse)
+                                                                                                                                      
+#Prediction
+prediction = esn.predict(pred_length)
+
+plt.figure()
+plt.title("Prediction of the continuation")
+plt.xlabel("time")
+plt.plot(time_future, prediction, color="green")
+
+plt.show()
+
+
---
+++ example_sin.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 12:33:35 2020
+
+@author: aleix
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from echo_state_networks.standard import StdEsn
+
+
+time = np.linspace(0, 200, 2000)
+sinus = np.sin(time)
+
+time_step = time[1] - time[0]
+pred_length = 1000
+future_time = [time[-1] + i*time_step for i in range(1, pred_length+1)]
+
+plt.figure()
+plt.title("Sinus Training signal")
+plt.plot(time, sinus)
+
+#Echo state network construction with default parameters
+esn = StdEsn()
+#Training
+xstates, train_mse = esn.teacher_forcing(sinus)
+print("training MSE = ", train_mse)
+
+#Prediction
+prediction = esn.predict(pred_length)
+
+plt.figure()
+plt.title("Prediction of the continuation")
+plt.plot(future_time, prediction, color="green")
+
